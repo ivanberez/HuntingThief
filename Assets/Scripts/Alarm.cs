06513bdf7556fc0e398ad46761e90244ee2c242f
ivanberez <berejnoyi@mail.ru>
2024-03-13 14:40:23 +0800

    Changes after 1 attempts

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -1,27 +1,57 @@
 using UnityEngine;
 using DG.Tweening;
 
+[RequireComponent(typeof(AudioSource))]
 public class Alarm : MonoBehaviour
 {
-    private const float Duration = 1f;
-    private const float Strength = 1f;
+    private const float MaxVolume = 1f;
+    private const float MinVolume = 0f;
+    private const float DurationShake = 1f;
+    private const float StrengthShake = 1f;
 
+    [Range(1, 3)]
+    [SerializeField] private float _durationChangeSound;
+    [SerializeField] private AlarmZone _alarmZone;    
+    
+    private AudioSource _audioSource;    
     private Vector3 _normalScale;
-    private Tween _tween;
+    private Tween _tween;    
 
     private void Awake()
-    {
-        _normalScale = transform.localScale;
+    {         
+        _audioSource = GetComponent<AudioSource>();
+        _normalScale = transform.localScale;        
     }
 
-    public void Run()
+    private void OnEnable()
     {
-        _tween = transform.DOShakeScale(Duration, Strength).From().SetLoops(-1, LoopType.Yoyo);
+        _alarmZone.Alerting += Switching;
     }
 
-    public void Stop() 
+    private void OnDisable()
+    {
+       _alarmZone.Alerting -= Switching;
+    }
+
+    private void Switching(bool isAlert)
+    {
+        if (isAlert)
+            Run();
+        else
+            Stop();
+    }
+
+    private void Run()
+    {
+        _tween = transform.DOShakeScale(DurationShake, StrengthShake).From().SetLoops(-1, LoopType.Yoyo);        
+        _audioSource.Play();
+        _audioSource.DOFade(MaxVolume, _durationChangeSound);
+    }
+
+    private void Stop() 
     {
         _tween.Kill();
+        _audioSource.DOFade(MinVolume, _durationChangeSound).OnComplete(() => _audioSource.Stop());
         transform.localScale = _normalScale;
     }
 }
--- Assets/Scripts/AlarmZone.cs
+++ Assets/Scripts/AlarmZone.cs
@@ -1,45 +1,21 @@
+using System;
 using UnityEngine;
-using DG.Tweening;
 
 public class AlarmZone : MonoBehaviour
 {
-    private const float MaxVolume = 1;
-    private const float MinVolume = 0;
-
-    [SerializeField] private string _targetTag = "Thief";
-    [SerializeField] private Alarm[] _alarms;    
+    [SerializeField] private Alarm[] _alarms;
     
-    [Header("Sound")]
-    [SerializeField] private AudioSource _audio;    
-    [Range(1, 5)]
-    [SerializeField] private float _durationChangeSound;
-
-    private void StartAlert()
-    {
-        foreach (Alarm alarm in _alarms)
-            alarm.Run();
-
-        _audio.Play();
-        _audio.DOFade(MaxVolume, _durationChangeSound);
-    }
-
-    private void StopAlert()
-    {
-        foreach (Alarm alarm in _alarms)
-            alarm.Stop();
-
-        _audio.DOFade(MinVolume, _durationChangeSound).OnComplete(()=> _audio.Stop());
-    }
+    public Action<bool> Alerting;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == _targetTag)
-            StartAlert();
+        if (collision.TryGetComponent(out Thief thief))
+            Alerting?.Invoke(true);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.tag == _targetTag)
-            StopAlert();
-    }    
+        if (collision.TryGetComponent(out Thief thief))
+            Alerting?.Invoke(false);
+    }
 }
